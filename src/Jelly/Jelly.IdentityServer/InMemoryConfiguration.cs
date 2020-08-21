5d70b38d22cede4843761be18db1d6076d7fe872
刘家栋 <jelly.liut@gmail.com>
2020-08-21 18:01:47 +0800

    IdentityServer 暂时无法登录

---
+++ src/Jelly/Jelly.IdentityServer/InMemoryConfiguration.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace Jelly.IdentityServer
+{
+    public class InMemoryConfiguration
+    {
+        /// <summary>
+        /// Api Scopes
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiScope> ApiScopes()
+        {
+            return new List<ApiScope>
+            {
+                new ApiScope("scope1","scope1")
+            };
+        }
+        /// <summary>
+        /// ApiResources
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[]
+            {
+                new ApiResource
+                {
+                    Name = "api1",
+                    DisplayName = "My Api1",
+                    Scopes = { "scope1" }
+                }
+            };
+        }
+        /// <summary>
+        /// Clients
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> Clients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "scope1" }
+                }
+            };
+        }
+        /// <summary>
+        /// Users
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[]
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "mail@qq.com",
+                    Password = "password"
+                }
+            };
+        }
+    }
+}
---
+++ src/Jelly/Jelly.IdentityServer/Startup.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Jelly.IdentityServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        { 
+            services.AddControllersWithViews();
+
+            #region IdentityServer
+            //首次启动时，Identity Server4将创建一个开发人员签名密钥，该文件名为tempkey.rsa。不必将该文件签入源代码管理中，如果不存在该文件将被重新创建。也就是AddDeveloperSigningCredential()。 这个方法只适合用于Identity Server4在单个机器运行, 如果是生产环境你得使用AddSigningCredential()这个方法.
+
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddTestUsers(InMemoryConfiguration.Users().ToList())
+                .AddInMemoryClients(InMemoryConfiguration.Clients())
+                .AddInMemoryApiScopes(InMemoryConfiguration.ApiScopes())
+                .AddInMemoryApiResources(InMemoryConfiguration.ApiResources());
+            #endregion
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+        }
+    }
+}
