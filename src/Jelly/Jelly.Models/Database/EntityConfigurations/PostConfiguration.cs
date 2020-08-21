138de5ead56a745eb69145a4bf3b1ed640fbff8e
刘家栋 <jelly.liut@gmail.com>
2020-08-21 17:20:20 +0800

    实体属性Fluent API地址

--- src/Jelly/Jelly.Models/Database/EntityConfigurations/PostConfiguration.cs
+++ src/Jelly/Jelly.Models/Database/EntityConfigurations/PostConfiguration.cs
@@ -3,6 +3,10 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Jelly.Models.Database.EntityConfigurations
 {
+    /// <summary>
+    /// 实体属性
+    /// 参考 https://docs.microsoft.com/zh-cn/ef/core/modeling/entity-properties
+    /// </summary>
     public class PostConfiguration : IEntityTypeConfiguration<Post>
     {
         public void Configure(EntityTypeBuilder<Post> builder)
