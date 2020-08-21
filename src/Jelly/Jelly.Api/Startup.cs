193d353f07e4e4a42199c8786a1658dd8b94392e
刘家栋 <jelly.liut@gmail.com>
2020-08-21 16:04:19 +0800

    FluentValidation

--- src/Jelly/Jelly.Api/Startup.cs
+++ src/Jelly/Jelly.Api/Startup.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using FluentValidation;
 using Jelly.Api.Extensions;
 using Jelly.Core.AutoMapper;
 using Jelly.Models.Database;
+using Jelly.Resources;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -37,6 +39,9 @@ namespace Jelly.Api
             });
             //AutoMapper
             services.AddAutoMapper(MapperRegister.MapType());
+
+            //FluentValidation
+            services.AddTransient<IValidator<PostResource>, PostResourceValidator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
