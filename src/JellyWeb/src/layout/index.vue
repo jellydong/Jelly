a55821f8700abdd40e4def3abc788261d505c2fd
刘家栋 <jelly.liut@gmail.com>
2020-09-03 17:38:46 +0800

    使用element-admin

---
+++ src/JellyWeb/src/layout/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="classObj" class="app-wrapper">
+    <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
+    <sidebar class="sidebar-container" />
+    <div class="main-container">
+      <div :class="{'fixed-header':fixedHeader}">
+        <navbar />
+      </div>
+      <app-main />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
