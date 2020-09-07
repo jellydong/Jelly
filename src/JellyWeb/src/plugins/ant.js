4e081ccbe432c0b640f37136f66e69d7a6817214
刘家栋 <jelly.liut@gmail.com>
2020-09-07 13:35:34 +0800

    ant

--- src/JellyWeb/src/plugins/ant.js
+++ src/JellyWeb/src/plugins/ant.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 
 import {
   message,
+  notification,
   Modal
 } from 'ant-design-vue'
 
 Vue.use(Modal)
 
 Vue.prototype.$message = message
+Vue.prototype.$notification = notification
 Vue.prototype.$info = Modal.info
 Vue.prototype.$success = Modal.success
 Vue.prototype.$error = Modal.error
